ae46f24a95241c00f0bde795a211623db6351c34
veselints <veselintsvetanov@gmail.com>
2021-02-13 12:47:23 +0200

    feat: init Perfect channel project

---
+++ Backend/PerfectChannel.WebApi.Test/Controllers/TaskController.Tests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+public class TaskControllerTests
+{
+    public TaskControllerTests()
+    {
+    } 
+
+    [Test]
+    public async Task SampleTest()
+    {
+        // Act
+        
+
+        // Assert
+        Assert.AreEqual("true", "false");
+    }
+}
---
+++ Backend/PerfectChannel.WebApi/Controllers/TaskController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PerfectChannel.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("AllowOrigin")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        public ActionResult Get()
+        {
+            return new JsonResult(new { test = "hop" });
+        }
+        // TODO: to be completed
+    }
+}
